d139fd7ecac419b5c15d190c97429dda97f7815d
cgrountree <cgrountree@gmail.com>
2021-07-18 01:11:28 -0400

    update styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,28 +3,26 @@ import React, { Component } from 'react';
 function Home() {
     return (
         <div>
-            <header>
-                <div>
-                    <h1>dork squad</h1>
-                    <h3>
-                        welcome to the dork squad hub
-                    </h3>
-                </div>
-            </header>
             <body>
-                <div>
+                <div class="text-center">
+                    <h2>
+                        welcome to the dork squad hub
+                    </h2>
+                </div>
+                <div class="text-center max-heights">
                     <p>
                         we make music and art. use this hub to explore everything we've ever made.
                     </p>
+                    <div class="padding-top-1">
+                        <button>explore</button>
+                    </div>
                 </div>
-            </body>
-            <footer>
-                <div>
+                <div class="footer text-center">
                     <p>
                         if you want to contact us <a href="/">click here</a>
                     </p>
                 </div>
-            </footer>
+            </body>
         </div>
     );
 }
