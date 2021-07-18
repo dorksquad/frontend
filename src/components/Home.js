d2cde63cb96e5798844cba0132842b48f5d25c99
cgrountree <cgrountree@gmail.com>
2021-07-17 23:24:35 -0400

    add home component with basic content, no styling

---
+++ src/components/Home.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+function Home() {
+    return (
+        <div>
+            <header>
+                <div>
+                    <h1>dork squad</h1>
+                    <h3>
+                        welcome to the dork squad hub
+                    </h3>
+                </div>
+            </header>
+            <body>
+                <div>
+                    <p>
+                        we make music and art. use this hub to explore everything we've ever made.
+                    </p>
+                </div>
+            </body>
+            <footer>
+                <div>
+                    <p>
+                        if you want to contact us <a href="/">click here</a>
+                    </p>
+                </div>
+            </footer>
+        </div>
+    );
+}
+
+export default Home;
