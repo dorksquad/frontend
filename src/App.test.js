7871b55bae8f63e17c428b69e615ad61b1191bdc
cgrountree <cgrountree@gmail.com>
2021-07-23 22:16:14 -0400

    update test description

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders home page', () => {
   render(<App />);
   const linkElement = screen.getByText(/dork squad/i);
   expect(linkElement).toBeInTheDocument();
