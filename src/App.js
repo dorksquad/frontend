c9a7cf40d31f3a69028d08b3d7c6dd7449b42bbe
cgrountree <cgrountree@gmail.com>
2021-07-23 21:56:19 -0400

    fix flashing of background and move resources

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
-  
+
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <Home />
   );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,26 +3,24 @@ import React, { Component } from 'react';
 function Home() {
     return (
         <div>
-            <body>
-                <div class="text-center">
-                    <h2>
-                        welcome to the dork squad hub
-                    </h2>
-                </div>
-                <div class="text-center">
-                    <p>
-                        we make music and art. use this hub to explore everything we've ever made.
-                    </p>
-                </div>
-                <div class="text-center padding-top-1">
-                    <button>explore</button>
-                </div>
-                <div class="footer text-right">
-                    <p>
-                        if you want to contact us <a href="/">click here</a>
-                    </p>
-                </div>
-            </body>
+            <div className="text-center">
+                <h2>
+                    welcome to the dork squad hub
+                </h2>
+            </div>
+            <div className="text-center">
+                <p>
+                    we make music and art. use this hub to explore everything we've ever made.
+                </p>
+            </div>
+            <div className="text-center padding-top-1">
+                <button>explore</button>
+            </div>
+            <div className="footer text-right">
+                <p>
+                    if you want to contact us <a href="/">click here</a>
+                </p>
+            </div>
         </div>
     );
 }
