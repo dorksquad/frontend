43b3e71aad795e5e5eeb3226ac4c105c65ad4c36
cgrountree <cgrountree@gmail.com>
2021-07-26 19:56:07 -0400

    add eslint and prettier, format, and update readme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import React, { Component } from 'react';
-import Home from './components/Home';
+import React from "react";
+import Home from "./components/Home";
 
 function App() {
-  return (
-    <Home />
-  );
+  return <Home />;
 }
 
 export default App;
--- src/api/Api.test.js
+++ src/api/Api.test.js
@@ -1,89 +1,89 @@
-import Api from './Api';
+import Api from "./Api";
 
-// two Apis initialized. one with auth and one without. 
+// two Apis initialized. one with auth and one without.
 // this will be used more later when we actually implement auth
-const apiWithAuthToken = new Api('auth token');
+const apiWithAuthToken = new Api("auth token");
 const apiWithoutAuthToken = new Api();
 
 // these tests do not actually test end to end. do we want to test end to end here?
-describe('getArtworks function', () => {
-    it('auth token present fetch is called', async () => {
-       global.fetch = jest.fn(() => {
-          Promise.resolve();
-       });
-       await apiWithAuthToken.getArtworks();
-       expect(fetch).toHaveBeenCalledTimes(1);
+describe("getArtworks function", () => {
+  it("auth token present fetch is called", async () => {
+    const fetch = jest.fn(() => {
+      Promise.resolve();
     });
-    it('auth token not present fetch is called', async () => {
-        global.fetch = jest.fn(() => {
-           Promise.resolve();
-        });
-        await apiWithoutAuthToken.getArtworks();
-        expect(fetch).toHaveBeenCalledTimes(1);
-     });
+    await apiWithAuthToken.getArtworks();
+    expect(fetch).toHaveBeenCalledTimes(1);
+  });
+  it("auth token not present fetch is called", async () => {
+    const fetch = jest.fn(() => {
+      Promise.resolve();
+    });
+    await apiWithoutAuthToken.getArtworks();
+    expect(fetch).toHaveBeenCalledTimes(1);
+  });
 });
 
-describe('getArtworkByName function', () => {
-    it('auth token present fetch is called', async () => {
-       global.fetch = jest.fn(() => {
-          Promise.resolve();
-       });
-       const name = 'chill name';
-       await apiWithAuthToken.getArtworkByName(name);
-       expect(fetch).toHaveBeenCalledTimes(1);
+describe("getArtworkByName function", () => {
+  it("auth token present fetch is called", async () => {
+    const fetch = jest.fn(() => {
+      Promise.resolve();
     });
-    it('auth token not present fetch is called', async () => {
-        global.fetch = jest.fn(() => {
-           Promise.resolve();
-        });
-        const name = 'chill name';
-        await apiWithoutAuthToken.getArtworkByName(name);
-        expect(fetch).toHaveBeenCalledTimes(1);
-     });
+    const name = "chill name";
+    await apiWithAuthToken.getArtworkByName(name);
+    expect(fetch).toHaveBeenCalledTimes(1);
+  });
+  it("auth token not present fetch is called", async () => {
+    const fetch = jest.fn(() => {
+      Promise.resolve();
+    });
+    const name = "chill name";
+    await apiWithoutAuthToken.getArtworkByName(name);
+    expect(fetch).toHaveBeenCalledTimes(1);
+  });
 });
 
-describe('getArtworkByAlbum function', () => {
-    it('auth token present fetch is called', async () => {
-       global.fetch = jest.fn(() => {
-          Promise.resolve();
-       });
-       const album = 'chill album';
-       await apiWithAuthToken.getArtworkByAlbum(album);
-       expect(fetch).toHaveBeenCalledTimes(1);
+describe("getArtworkByAlbum function", () => {
+  it("auth token present fetch is called", async () => {
+    const fetch = jest.fn(() => {
+      Promise.resolve();
     });
-    it('auth token not present fetch is called', async () => {
-        global.fetch = jest.fn(() => {
-           Promise.resolve();
-        });
-        const album = 'chill album';
-        await apiWithoutAuthToken.getArtworkByAlbum(album);
-        expect(fetch).toHaveBeenCalledTimes(1);
-     });
+    const album = "chill album";
+    await apiWithAuthToken.getArtworkByAlbum(album);
+    expect(fetch).toHaveBeenCalledTimes(1);
+  });
+  it("auth token not present fetch is called", async () => {
+    const fetch = jest.fn(() => {
+      Promise.resolve();
+    });
+    const album = "chill album";
+    await apiWithoutAuthToken.getArtworkByAlbum(album);
+    expect(fetch).toHaveBeenCalledTimes(1);
+  });
 });
 
-describe('addArtwork function', () => {
-    it('auth token present fetch is called', async () => {
-       global.fetch = jest.fn(() => {
-          Promise.resolve();
-       });
-       const artwork = {
-           name: 'chill name',
-           album: 'chill album',
-           file: '0101010101010'
-       };
-       await apiWithAuthToken.addArtwork(artwork);
-       expect(fetch).toHaveBeenCalledTimes(1);
+describe("addArtwork function", () => {
+  it("auth token present fetch is called", async () => {
+    const fetch = jest.fn(() => {
+      Promise.resolve();
     });
-    it('auth token not present fetch is called', async () => {
-        global.fetch = jest.fn(() => {
-           Promise.resolve();
-        });
-        const artwork = {
-            name: 'chill name',
-            album: 'chill album',
-            file: '0101010101010'
-        };
-        await apiWithoutAuthToken.addArtwork(artwork);
-        expect(fetch).toHaveBeenCalledTimes(1);
-     });
+    const artwork = {
+      name: "chill name",
+      album: "chill album",
+      file: "0101010101010",
+    };
+    await apiWithAuthToken.addArtwork(artwork);
+    expect(fetch).toHaveBeenCalledTimes(1);
+  });
+  it("auth token not present fetch is called", async () => {
+    const fetch = jest.fn(() => {
+      Promise.resolve();
+    });
+    const artwork = {
+      name: "chill name",
+      album: "chill album",
+      file: "0101010101010",
+    };
+    await apiWithoutAuthToken.addArtwork(artwork);
+    expect(fetch).toHaveBeenCalledTimes(1);
+  });
 });
